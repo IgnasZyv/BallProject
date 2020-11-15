5b92eeedc1a75a8839fd7064a947aa9da6291cdb
IgnasZyv <74272405+IgnasZyv@users.noreply.github.com>
2020-11-15 00:32:16 +0000

    Update Bag.java

--- BallProject/Bag.java
+++ BallProject/Bag.java
@@ -16,22 +16,23 @@ public class Bag {
     public static void main(String[] args) {
         Balls whiteBalls = new Balls("White");
         Balls blackBalls = new Balls("Black");
+        Balls[] objArray;
         whiteBalls.setAmount(16);
         blackBalls.setAmount(20);
         int totalBalls = whiteBalls.getAmount() + blackBalls.getAmount();
         //Create two arrays that hold white and black balls together.
-        // Adding white balls into array "white".
-        ArrayList<Balls> white = new ArrayList<>();
+            // Adding white balls into array "white".
+        ArrayList<Balls> white = new ArrayList<Balls>();
         for (int i = 0; i < whiteBalls.getAmount(); i++) {
             white.add(whiteBalls);
             //System.out.println(i);
         }       // Adding black balls into array "black".
-        ArrayList<Balls> black = new ArrayList<>();
+        ArrayList<Balls> black = new ArrayList<Balls>();
         for (int i = 0; i < blackBalls.getAmount(); i++) {
             black.add(blackBalls);
             //System.out.println(i);
         }    // New Array which has all contents of arrays white and black.
-        ArrayList<Balls> bag = new ArrayList<>(totalBalls);
+        ArrayList<Balls> bag = new ArrayList<Balls>(totalBalls);
         bag.addAll(0, white);
         bag.addAll(15, black);
 
@@ -41,9 +42,9 @@ public class Bag {
         }*/
 
         int count = 0;
-        Random rnd = new Random();
 // Depending on what colour the balls are either black ball is placed or white.
         while (bag.size() > 1) {
+            Random rnd = new Random();
             int select1 = rnd.nextInt(bag.size());
             int select2 = rnd.nextInt(bag.size());
             System.out.println(select1);
@@ -51,28 +52,21 @@ public class Bag {
             Balls indexPos1 = bag.get(select1);
             Balls indexPos2 = bag.get(select2);
             count++;
-            if (bag.size() != 1) {
-                System.out.println(bag);
-                System.out.println("Printing size: " + bag.size());
-                if (bag.get(select1).equals(blackBalls) && indexPos2.equals(blackBalls)) {
+            if(bag.size() != 1){
+                if (bag.get(select1).equals(blackBalls) && indexPos2.equals(blackBalls)){
                     System.out.println("Both balls are black.");
-                    System.out.println("Removing black ball");
-                    bag.remove(blackBalls);
-                    bag.remove(blackBalls);
-                    bag.add(blackBalls);
+                    bag.remove(indexPos1);
                     continue;
+
                 }if (indexPos1.equals(whiteBalls) && indexPos2.equals(whiteBalls)) {
                     System.out.println("both Balls are white");
-                    bag.remove(whiteBalls);
-                    bag.remove(whiteBalls);
+                    bag.remove(indexPos1);
+                    bag.remove(indexPos1);
                     bag.add(blackBalls);
-                    System.out.println("Removing 2 white balls. adding black.");
-                }if (indexPos1.equals(whiteBalls) && indexPos2.equals(blackBalls) || indexPos1.equals(blackBalls) && indexPos2.equals(whiteBalls)) {
-                    bag.remove(select1);
-                    bag.remove(select2);
-                    bag.add(whiteBalls);
+
+                }else{
                     System.out.println("Both balls are different.");
-                    System.out.println("Removing white ball");
+                    bag.remove(indexPos2);
                 }
             }
         }
@@ -80,7 +74,5 @@ public class Bag {
         System.out.println("Count is : " + count);
     }
 }
-
-
 
 
