60a35044d99035cc8c443a1bb01c6e5685d479f9
IgnasZyv <74272405+IgnasZyv@users.noreply.github.com>
2020-11-15 21:52:20 +0000

    Update Bag.java

--- BallProject/Bag.java
+++ BallProject/Bag.java
@@ -14,77 +14,93 @@ public class Bag {
 
 
     public static void main(String[] args) {
-        Balls whiteBalls = new Balls("White");
-        Balls blackBalls = new Balls("Black");
-        whiteBalls.setAmount(16);
-        blackBalls.setAmount(20);
+
+//CREATING 2 OBJECTS, WHITE & BLACK
+        Balls whiteBalls = new Balls("White", 16);
+        Balls blackBalls = new Balls("Black", 20);
+
+//ADDING THE VALUES OF BLACK-BALL AND WHITE-BALL TO AN INT CALLED "TOTALBALLS"
         int totalBalls = whiteBalls.getAmount() + blackBalls.getAmount();
-        //Create two arrays that hold white and black balls together.
-        // Adding white balls into array "white".
+
+//ASSIGNING THE VALUE OF WHITE-BALL TO AN ARRAYLIST CALLED "WHITE-BALL"
         ArrayList<Balls> white = new ArrayList<>();
         for (int i = 0; i < whiteBalls.getAmount(); i++) {
             white.add(whiteBalls);
-            //System.out.println(i);
-        }       // Adding black balls into array "black".
+        }
+
+//ASSIGNING THE VALUE OF BLACK-BALL TO AN ARRAYLIST CALLED "BLACK-BALL"
         ArrayList<Balls> black = new ArrayList<>();
         for (int i = 0; i < blackBalls.getAmount(); i++) {
             black.add(blackBalls);
-            //System.out.println(i);
-        }    // New Array which has all contents of arrays white and black.
+        }
+
+//CREATING NEW "BAG" ARRAYLIST AND ASSIGNING THE VALUES OF "INT TOTALBALLS"
         ArrayList<Balls> bag = new ArrayList<>(totalBalls);
+
+//INDEX 0 = WHITE-BALL, INDEX 16 = BLACK-BALL
         bag.addAll(0, white);
         bag.addAll(16, black);
-        /*
-       System.out.println("Printing contents of bag.");
-        for (Balls i : bag) {
-            System.out.println("ball: " + i);
-        }
-        */
+
+//COUNT TOTAL BALLS
         int count = 0;
+
+//DECLARING A RANDOM OBJECT
         Random rnd = new Random();
-// Depending on what colour the balls are either black ball is placed or white.
+
+//IF THE BAG-ARRAY IS MORE THAN 1, "INT COUNT" WILL INCREMENT
         while (bag.size() > 1) {
             count++;
+//SELECTING 2 RANDOM BALLS FROM THE BAG
             int select1 = rnd.nextInt(bag.size());
-            Balls indexPos1 = bag.get(select1);
             int select2 = rnd.nextInt(bag.size());
             while (select2 == select1) {
                 select2 = rnd.nextInt(bag.size());
             }
-            Balls indexPos2 = bag.get(select2);
-            System.out.println(select1);
-            System.out.println(select2);
+
+//IF THE BAG DOES NOT EQUAL 1: DO THIS
             if (bag.size() != 1) {
-                System.out.println(bag);
-                System.out.println("Printing size: " + bag.size());
-                if (indexPos1.equals(blackBalls) && indexPos2.equals(blackBalls)) {
-                    System.out.println("Both balls are black.");
-                    System.out.println("Removing black ball");
+                System.out.println("Amount of Balls left: " + bag.size());
+
+//IF - SELECT 1 & 2 ARE BOTH BLACK-BALLS: DO THIS
+                if (bag.get(select1).equals(blackBalls) && bag.get(select2).equals(blackBalls)) {
+                    System.out.println("You got 2 Blackballs.");
                     bag.remove(blackBalls);
                     bag.remove(blackBalls);
                     bag.add(blackBalls);
+                    System.out.println("Removing 2 Blackballs, Adding Blackball");
+                    System.out.println("");
                     continue;
-                }if (indexPos1.equals(whiteBalls) && indexPos2.equals(whiteBalls)) {
-                    System.out.println("both Balls are white");
+                }
+
+//IF - SELECT 1 & 2 ARE BOTH WHITE-BALLS: DO THIS
+                if (bag.get(select1).equals(whiteBalls) && bag.get(select2).equals(whiteBalls)) {
+                    System.out.println("You got 2 Whiteballs");
                     bag.remove(whiteBalls);
                     bag.remove(whiteBalls);
                     bag.add(blackBalls);
-                    System.out.println("Removing 2 white balls. adding black.");
+                    System.out.println("Removing 2 Whiteballs. Adding Blackball.");
+                    System.out.println("");
                     continue;
-                }if (indexPos1.equals(whiteBalls) && indexPos2.equals(blackBalls) || indexPos1.equals(blackBalls) && indexPos2.equals(whiteBalls)) {
+                }
+
+//IF - SELECT 1 & 2 ARE WHITE AND BLACK OR BLACK AND WHITE: DO THIS
+                if (bag.get(select1).equals(whiteBalls) && bag.get(select2).equals(blackBalls)
+                        || bag.get(select1).equals(blackBalls) && bag.get(select2).equals(whiteBalls)) {
                     bag.remove(whiteBalls);
                     bag.remove(blackBalls);
                     bag.add(whiteBalls);
                     System.out.println("Both balls are different.");
-                    System.out.println("Removing white ball");
+                    System.out.println("Removing White and Black balls. Adding White");
+                    System.out.println("");
+
                 }
             }
         }
+        System.out.println("BAG SIZE: " + bag.size());
         System.out.println("The last remaining ball is: " + bag);
         System.out.println("Count is : " + count);
     }
 }
-
 
 
 
